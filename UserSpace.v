90eda4cbc8a20587788728deef30fbfaefff2eab
Wevel <charlie.david.smith@hotmail.co.uk>
2023-05-17 14:29:43 +0100

    Added corePC testbench that wraps only the userspace, rather than the entire chip. This runs significantly faster.

---
+++ UserSpace.v
@@ -0,0 +1,283 @@
+`timescale 1 ns / 1 ps
+
+`define GPIO0_OE_WRITE_ADDR 32'h33031000
+`define GPIO0_OE_SET_ADDR 32'h33031004
+`define GPIO0_OE_CLEAR_ADDR 32'h33031008
+`define GPIO0_OE_TOGGLE_ADDR 32'h3303100C
+`define GPIO0_OUTPUT_WRITE_ADDR 32'h33031010
+`define GPIO0_OUTPUT_SET_ADDR 32'h33031014
+`define GPIO0_OUTPUT_CLEAR_ADDR 32'h33031018
+`define GPIO0_OUTPUT_TOGGLE_ADDR 32'h3303101C
+`define GPIO0_INPUT_ADDR 32'h33031020
+`define GPIO1_OE_WRITE_ADDR 32'h33032000
+`define GPIO1_OE_SET_ADDR 32'h33032004
+`define GPIO1_OE_CLEAR_ADDR 32'h33032008
+`define GPIO1_OE_TOGGLE_ADDR 32'h3303200C
+`define GPIO1_OUTPUT_WRITE_ADDR 32'h33032010
+`define GPIO1_OUTPUT_SET_ADDR 32'h33032014
+`define GPIO1_OUTPUT_CLEAR_ADDR 32'h33032018
+`define GPIO1_OUTPUT_TOGGLE_ADDR 32'h3303201C
+`define GPIO1_INPUT_ADDR 32'h33032020
+
+`define CORE0_SRAM_ADDR 32'h30000000
+`define CORE0_CONFIG_ADDR 32'h30800000
+`define CORE0_REG_PC_ADDR 32'h30810000
+`define CORE0_REG_JUMP_ADDR 32'h30810004
+`define CORE0_REG_STEP_ADDR 32'h30810008
+`define CORE0_REG_INSTR_ADDR 32'h30810010
+`define CORE0_REG_IREG_ADDR 32'h30814000
+`define CORE0_CSR_ADDR 32'h30818000
+
+`define CORE1_SRAM_ADDR 32'h31000000
+`define CORE1_CONFIG_ADDR 32'h31800000
+`define CORE1_REG_PC_ADDR 32'h31810000
+`define CORE1_REG_JUMP_ADDR 32'h31810004
+`define CORE1_REG_STEP_ADDR 32'h31810008
+`define CORE1_REG_INSTR_ADDR 32'h31810010
+`define CORE1_REG_IREG_ADDR 32'h31814000
+`define CORE1_CSR_ADDR 32'h31818000
+
+`define CORE_RUN 32'h1
+`define CORE_HALT 32'h0
+
+`define RV32I_NOP 32'h00000013
+`define RV32I_JMP_PREV 32'hFFDFF06F
+
+`define SELECT_WORD 4'b1111
+`define SELECT_HALF 4'b0011
+`define SELECT_BYTE 4'b0001
+
+`define OFFSET_WORD(BASE_ADDRESS, OFFSET) (BASE_ADDRESS) + {(OFFSET), 2'b00}
+
+`define WB_WRITE_RAW(ADDRESS, BYTE_SELECT, DATA) \
+	@(negedge clk) \ // Wait till before clock rising edge
+	#1 \
+	wait(!wbBusy); \ // Wait for bus to be free
+	wbAddress <= ADDRESS; \ // Setup write
+	wbByteSelect <= BYTE_SELECT; \
+	wbWriteEnable <= 1'b1; \
+	wbDataWrite <= DATA; \
+	wbEnable <= 1'b1; \ // Start write
+	@(posedge clk) \ // Wait for write to complete
+	@(posedge clk) \
+	@(posedge clk) \
+	wait(!wbBusy);
+
+`define WB_READ_RAW(ADDRESS, BYTE_SELECT, DATA) \
+	@(negedge clk) \ // Wait till before clock rising edge
+	#1 \
+	wait(!wbBusy); \ // Wait for bus to be free
+	wbAddress <= ADDRESS; \ // Setup read
+	wbByteSelect <= BYTE_SELECT; \
+	wbWriteEnable <= 1'b0; \
+	wbEnable <= 1'b1; \ // Start read
+	@(posedge clk) \ // Wait for read to complete
+	@(posedge clk) \
+	@(posedge clk) \
+	wait(!wbBusy); \
+	DATA <= wbDataRead; // Return data
+
+`define WB_WRITE(ADDRESS, BYTE_SELECT, DATA) \
+	`WB_WRITE_RAW(32'h30000000, `SELECT_WORD, (ADDRESS) & 32'hFFFF8000) \ // Set top part of address
+	`WB_WRITE_RAW(((ADDRESS) & 32'h00007FFF) | 32'h30008000, BYTE_SELECT, DATA) // Perform write
+
+`define WB_READ(ADDRESS, BYTE_SELECT, DATA) \
+	`WB_WRITE_RAW(32'h30000000, `SELECT_WORD, (ADDRESS) & 32'hFFFF8000) \ // Set top part of address
+	`WB_READ_RAW(((ADDRESS) & 32'h00007FFF) | 32'h30008000, BYTE_SELECT, DATA) // Perform read
+
+`define TEST_RESULT(RESULT) \
+	@(negedge clk) \ // Wait till before clock rising edge
+	#1 \
+	if (succesOutput) succesOutput <= (RESULT);\
+	nextTestOutput <= 1'b1;\
+	@(posedge clk); \ // Wait for a clock cycle
+	@(negedge clk); \
+	nextTestOutput <= 1'b0;
+
+`define TEST_WRITE(ADDRESS, BYTE_SELECT, DATA, EXPECTED_RESULT) \
+	`WB_WRITE(ADDRESS, BYTE_SELECT, EXPECTED_RESULT) \
+	`WB_READ(ADDRESS, BYTE_SELECT, DATA) \
+	`TEST_RESULT(DATA == (EXPECTED_RESULT))
+
+`define TEST_READ(ADDRESS, BYTE_SELECT, DATA, EXPECTED_RESULT) \
+	`WB_READ(ADDRESS, BYTE_SELECT, DATA) \
+	`TEST_RESULT(DATA == (EXPECTED_RESULT))
+
+`define TIMEOUT(TIME) \
+	repeat (TIME) begin \ // Repeat cycles of 1000 clock edges as needed to complete testbench
+		repeat (1000) @(posedge clk); \
+	end \
+	$display("%c[1;35m",27); \
+	`ifdef GL \
+		$display ("Monitor: Timeout, Core PC Test (GL) Failed"); \
+	`else \
+		$display ("Monitor: Timeout, Core PC Test (RTL) Failed"); \
+	`endif \
+	$display("%c[0m",27); \
+
+`define TESTS_COMPLETED \
+	#100 \
+	if (succesOutput) begin \
+		$display("%c[1;92m",27); \
+		`ifdef GL \
+			$display("Monitor: Core PC Test (GL) Passed"); \
+		`else \
+			$display("Monitor: Core PC Test (RTL) Passed"); \
+		`endif \
+		$display("%c[0m",27); \
+	end else begin \
+		$display("%c[1;31m",27); \
+		`ifdef GL \
+			$display ("Monitor: Core PC Test (GL) Failed"); \
+		`else \
+			$display ("Monitor: Core PC Test (RTL) Failed"); \
+		`endif \
+		$display("%c[0m",27); \
+	end
+
+module UserSpace(
+		input wire clk,
+		input wire rst,
+
+		input wire [127:0] la_data_in_user,
+		output wire [127:0] la_data_out_user,
+		input wire [127:0] la_oenb_user,
+
+		output wire [`MPRJ_IO_PADS-1:0] user_io_oeb,
+		input wire [`MPRJ_IO_PADS-1:0] user_io_in,
+		output wire [`MPRJ_IO_PADS-1:0] user_io_out,
+		inout wire [`MPRJ_IO_PADS-10:0] mprj_analog_io,
+
+		output wire[2:0] user_irq_core,
+
+		input wire[31:0] wbAddress,
+		input wire[3:0] wbByteSelect,
+		input wire wbEnable,
+		input wire wbWriteEnable,
+		input wire[31:0] wbDataWrite,
+		output wire[31:0] wbDataRead,
+		output wire wbBusy,
+
+		input wire succesOutput,
+		input wire nextTestOutput
+	);
+
+	// Dispaly message with result of each test, ending the simulation if a test fails
+	reg[31:0] testCounter = 0;
+	always @(succesOutput, nextTestOutput) begin
+		#1
+		if (nextTestOutput) begin
+			if (succesOutput) begin
+				$display("%c[1;92mPassed test: %d%c[0m", 27, testCounter, 27);
+			end	else begin
+				$display("%c[1;31mFailed test: %d%c[0m", 27, testCounter, 27);
+				#500
+				$finish;
+			end
+			testCounter <= testCounter + 1;
+		end
+	end
+
+	reg power1, power2;
+	reg power3, power4;
+
+	initial begin		// Power-up sequence
+		power1 <= 1'b0;
+		power2 <= 1'b0;
+		power3 <= 1'b0;
+		power4 <= 1'b0;
+		#100;
+		power1 <= 1'b1;
+		#100;
+		power2 <= 1'b1;
+		#100;
+		power3 <= 1'b1;
+		#100;	
+		power4 <= 1'b1;
+	end
+
+	wire VDD3V3;
+	wire VDD1V8;
+	wire VSS;
+	
+	assign VDD3V3 = power1;
+	assign VDD1V8 = power2;
+	assign VSS = 1'b0;
+
+	 // Exported Wishbone Bus (user area facing)
+	wire 	mprj_cyc_o_user;
+	wire 	mprj_stb_o_user;
+	wire 	mprj_we_o_user;
+	wire [3:0]  mprj_sel_o_user;
+	wire [31:0] mprj_adr_o_user;
+	wire [31:0] mprj_dat_o_user;
+	wire [31:0] mprj_dat_i_user;
+	wire	mprj_ack_i_user;
+	
+	Core_WBInterface #(.ADDRESS_WIDTH(32)) mprj_wb_interface (
+		// Wishbone master interface
+		.wb_clk_i(clk),
+		.wb_rst_i(rst),
+		.wb_cyc_o(mprj_cyc_o_user),
+		.wb_stb_o(mprj_stb_o_user),
+		.wb_we_o(mprj_we_o_user),
+		.wb_sel_o(mprj_sel_o_user),
+		.wb_data_o(mprj_dat_o_user),
+		.wb_adr_o(mprj_adr_o_user),
+		.wb_ack_i(mprj_ack_i_user),
+		.wb_stall_i(1'b0),
+		.wb_error_i(1'b0),
+		.wb_data_i(mprj_dat_i_user),
+
+		// Memory interface from core
+		.wbAddress(wbAddress),
+		.wbByteSelect(wbByteSelect),
+		.wbEnable(wbEnable),
+		.wbWriteEnable(wbWriteEnable),
+		.wbDataWrite(wbDataWrite),
+		.wbDataRead(wbDataRead),
+		.wbBusy(wbBusy)
+	);
+
+	user_project_wrapper mprj ( 
+		.vdda1(VDD3V3),		// User area 1 3.3V power
+		.vdda2(VDD3V3),		// User area 2 3.3V power
+		.vssa1(VSS),		// User area 1 analog ground
+		.vssa2(VSS),		// User area 2 analog ground
+		.vccd1(VDD1V8),		// User area 1 1.8V power
+		.vccd2(VDD1V8),		// User area 2 1.8V power
+		.vssd1(VSS),		// User area 1 digital ground
+		.vssd2(VSS),		// User area 2 digital ground
+
+		.wb_clk_i(clk),
+		.wb_rst_i(rst),
+
+		// Management SoC Wishbone bus (exported)
+		.wbs_cyc_i(mprj_cyc_o_user),
+		.wbs_stb_i(mprj_stb_o_user),
+		.wbs_we_i(mprj_we_o_user),
+		.wbs_sel_i(mprj_sel_o_user),
+		.wbs_adr_i(mprj_adr_o_user),
+		.wbs_dat_i(mprj_dat_o_user),
+		.wbs_ack_o(mprj_ack_i_user),
+		.wbs_dat_o(mprj_dat_i_user),
+
+		// GPIO pad 3-pin interface (plus analog)
+		.io_in (user_io_in),
+		.io_out(user_io_out),
+		.io_oeb(user_io_oeb),
+		.analog_io(mprj_analog_io),
+
+		// Logic analyzer
+		.la_data_in(la_data_in_user),
+		.la_data_out(la_data_out_user),
+		.la_oenb(la_oenb_user),
+
+		// Independent clock
+		.user_clock2(clk),
+
+		// IRQ
+		.user_irq(user_irq_core)
+	);
+
+endmodule
---
+++ user_corePC/user_corePC_tb.v
@@ -0,0 +1,197 @@
+`default_nettype none
+
+`timescale 1 ns / 1 ps
+
+`include "../UserSpace.v"
+
+module user_corePC_tb;
+	reg clk;
+	reg rst;
+
+	reg [127:0] la_data_in_user = 128'b0;  // From CPU to MPRJ
+	wire [127:0] la_data_out_user; // From MPRJ to CPU
+	reg [127:0] la_oenb_user = ~128'b0;	 // From CPU to MPRJ
+
+	wire [`MPRJ_IO_PADS-1:0] user_io_oeb;
+	reg [`MPRJ_IO_PADS-1:0] user_io_in = 'b0;
+	wire [`MPRJ_IO_PADS-1:0] user_io_out;
+	wire [`MPRJ_IO_PADS-10:0] mprj_analog_io;
+
+	reg[31:0] wbAddress = 32'b0;
+	reg[3:0] wbByteSelect = 4'b0;
+	reg wbEnable = 1'b0;
+	reg wbWriteEnable = 1'b0;
+	reg[31:0] wbDataWrite = 32'b0;
+	wire[31:0] wbDataRead;
+	wire wbBusy;
+
+	reg succesOutput = 1'b1;
+	reg nextTestOutput = 1'b0;
+
+	wire [2:0]   user_irq_core;
+
+	initial begin
+		$dumpfile("user_corePC.vcd");
+		$dumpvars(0, user_corePC_tb);
+		`TIMEOUT(100)
+		$finish;
+	end
+
+	reg[31:0] testValue = 32'b0;
+	reg[31:0] initialInstructionCount = 32'b0;
+	initial begin
+		@(negedge rst);
+		#100
+
+		// Run tests
+		// Setup test data in sram and make sure the test data has been written correctly
+		// If it isn't probably run a specific memory test, rather than this one
+		`TEST_WRITE(`CORE0_SRAM_ADDR, `SELECT_WORD, testValue, `RV32I_NOP)
+		`TEST_WRITE(`OFFSET_WORD(`CORE0_SRAM_ADDR, 32'h40), `SELECT_WORD, testValue, `RV32I_NOP)
+		`TEST_WRITE(`OFFSET_WORD(`CORE0_SRAM_ADDR, 32'h41), `SELECT_WORD, testValue, `RV32I_NOP)
+		`TEST_WRITE(`OFFSET_WORD(`CORE0_SRAM_ADDR, 32'h42), `SELECT_WORD, testValue, `RV32I_JMP_PREV)
+
+		`TEST_WRITE(`CORE1_SRAM_ADDR, `SELECT_WORD, testValue, `RV32I_NOP)
+		`TEST_WRITE(`OFFSET_WORD(`CORE1_SRAM_ADDR, 32'h40), `SELECT_WORD, testValue, `RV32I_NOP)
+		`TEST_WRITE(`OFFSET_WORD(`CORE1_SRAM_ADDR, 32'h41), `SELECT_WORD, testValue, `RV32I_NOP)
+		`TEST_WRITE(`OFFSET_WORD(`CORE1_SRAM_ADDR, 32'h42), `SELECT_WORD, testValue, `RV32I_JMP_PREV)
+
+		// Test core 0
+		// Read that the config defaulted to 0
+		`TEST_READ(`CORE0_CONFIG_ADDR, `SELECT_WORD, testValue, `CORE_HALT)
+
+		// Read that the program counter defaulted to 0x0000_0000
+		`TEST_READ(`CORE0_REG_PC_ADDR, `SELECT_WORD, testValue, 32'h0)
+
+		// Step PC
+		`WB_WRITE(`CORE0_REG_STEP_ADDR, `SELECT_WORD, 32'h0)
+
+		// Read that the PC stepped once
+		`TEST_READ(`CORE0_REG_PC_ADDR, `SELECT_WORD, testValue, 32'h4)
+
+		// Check that a NOP was read
+		`TEST_READ(`CORE0_REG_INSTR_ADDR, `SELECT_WORD, testValue, `RV32I_NOP)
+
+		// Jump PC
+		`WB_WRITE(`CORE0_REG_JUMP_ADDR, `SELECT_WORD, 32'h100)
+
+		// Read that the PC jumped correctly
+		`TEST_READ(`CORE0_REG_PC_ADDR, `SELECT_WORD, testValue, 32'h104)
+
+		// Let the core run
+		`WB_WRITE(`CORE0_CONFIG_ADDR, `SELECT_WORD, `CORE_RUN)
+		#200
+		`WB_WRITE(`CORE0_CONFIG_ADDR, `SELECT_WORD, `CORE_HALT)
+
+		// Make sure the core halted
+		`TEST_READ(`CORE0_CONFIG_ADDR, `SELECT_WORD, testValue, `CORE_HALT)
+
+		// Check that the core has run
+		`WB_READ(`OFFSET_WORD(`CORE0_CSR_ADDR, 30'hC02), `SELECT_WORD, initialInstructionCount)
+		`TEST_RESULT(initialInstructionCount > 1 && initialInstructionCount != ~32'h0)
+
+		// Let the core run
+		`WB_WRITE(`CORE0_CONFIG_ADDR, `SELECT_WORD, `CORE_RUN)
+		#200
+		`WB_WRITE(`CORE0_CONFIG_ADDR, `SELECT_WORD, `CORE_HALT)
+
+		// Make sure the core halted
+		`TEST_READ(`CORE0_CONFIG_ADDR, `SELECT_WORD, testValue, `CORE_HALT)
+
+		// Check that the core has been restarted
+		`WB_READ(`OFFSET_WORD(`CORE0_CSR_ADDR, 30'hC02), `SELECT_WORD, testValue)
+		`TEST_RESULT(testValue > initialInstructionCount)
+
+		// Check that the PC has increased should be either 0x104 or 0x108
+		`WB_READ(`CORE0_REG_PC_ADDR, `SELECT_WORD, testValue)
+		`TEST_RESULT(testValue == 32'h104 || testValue == 32'h108)
+
+		// Test core 1
+		// Read that the config defaulted to 0
+		`TEST_READ(`CORE1_CONFIG_ADDR, `SELECT_WORD, testValue, `CORE_HALT)
+
+		// Read that the program counter defaulted to 0x0000_0000
+		`TEST_READ(`CORE1_REG_PC_ADDR, `SELECT_WORD, testValue, 32'h0)
+
+		// Step PC
+		`WB_WRITE(`CORE1_REG_STEP_ADDR, `SELECT_WORD, 32'h0)
+
+		// Read that the PC stepped once
+		`TEST_READ(`CORE1_REG_PC_ADDR, `SELECT_WORD, testValue, 32'h4)
+
+		// Check that an NOP was read
+		`TEST_READ(`CORE1_REG_INSTR_ADDR, `SELECT_WORD, testValue, `RV32I_NOP)
+
+		// Jump PC
+		`WB_WRITE(`CORE1_REG_JUMP_ADDR, `SELECT_WORD, 32'h100)
+
+		// Read that the PC jumped correctly
+		`TEST_READ(`CORE1_REG_PC_ADDR, `SELECT_WORD, testValue, 32'h104)
+
+		// Let the core run
+		`WB_WRITE(`CORE1_CONFIG_ADDR, `SELECT_WORD, `CORE_RUN)
+		#200
+		`WB_WRITE(`CORE1_CONFIG_ADDR, `SELECT_WORD, `CORE_HALT)
+
+		// Make sure the core halted
+		`TEST_READ(`CORE1_CONFIG_ADDR, `SELECT_WORD, testValue, `CORE_HALT)
+
+		// Check that the core is running
+		`WB_READ(`OFFSET_WORD(`CORE1_CSR_ADDR, 30'hC02), `SELECT_WORD, initialInstructionCount)
+		`TEST_RESULT(initialInstructionCount > 1 && initialInstructionCount != ~32'h0)
+
+		// Let the core run
+		`WB_WRITE(`CORE1_CONFIG_ADDR, `SELECT_WORD, `CORE_RUN)
+		#200
+		`WB_WRITE(`CORE1_CONFIG_ADDR, `SELECT_WORD, `CORE_HALT)
+
+		// Make sure the core halted
+		`TEST_READ(`CORE1_CONFIG_ADDR, `SELECT_WORD, testValue, `CORE_HALT)
+
+
+		// Check that the core has been restarted
+		`WB_READ(`OFFSET_WORD(`CORE1_CSR_ADDR, 30'hC02), `SELECT_WORD, testValue)
+		`TEST_RESULT(testValue > initialInstructionCount)
+		
+		// Check that the PC has increased should be either 0x104 or 0x108
+		`WB_READ(`CORE1_REG_PC_ADDR, `SELECT_WORD, testValue)
+		`TEST_RESULT(testValue == 32'h104 || testValue == 32'h108)
+
+		`TESTS_COMPLETED
+	    $finish;
+	end
+
+	// External clock is used by default.  Make this artificially fast for the
+	// simulation.  Normally this would be a slow clock and the digital PLL
+	// would be the fast clock.
+	always #12.5 clk <= (clk === 1'b0);
+
+	initial begin
+		rst <= 1'b1;
+		#2000;
+		rst <= 1'b0; // Release reset
+	end
+
+	UserSpace userSpace(
+		.clk(clk),
+		.rst(rst),
+		.la_data_in_user(la_data_in_user),
+		.la_data_out_user(la_data_out_user),
+		.la_oenb_user(la_oenb_user),
+		.user_io_oeb(user_io_oeb),
+		.user_io_in(user_io_in),
+		.user_io_out(user_io_out),
+		.mprj_analog_io(mprj_analog_io),
+		.user_irq_core(user_irq_core),
+		.wbAddress(wbAddress),
+		.wbByteSelect(wbByteSelect),
+		.wbEnable(wbEnable),
+		.wbWriteEnable(wbWriteEnable),
+		.wbDataWrite(wbDataWrite),
+		.wbDataRead(wbDataRead),
+		.wbBusy(wbBusy),
+		.succesOutput(succesOutput),
+		.nextTestOutput(nextTestOutput));
+
+endmodule
+`default_nettype wire
